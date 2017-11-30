f302abafbd183224fd028455badd94de61c28a3a
Neil Castellino <neilcastellino@gmail.com>
2017-11-30 21:51:49 +0530

    Add files via upload

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,5 @@
+URL = "https://api.mailgun.net/v3/sandbox<api-key>.mailgun.org/messages"
+API_KEY = "key-<api-key>"
+FROM = "Mailgun Sandbox <postmaster@sandbox<api-key>.mailgun.org>"
+ALERT_TIMEOUT = 10
+COLLECTION = "alerts"
